a8eadf59c9ab3a8c2e529939996abb6ec4d9f2ee
renkarsu <42439654+renkarsu@users.noreply.github.com>
2021-02-09 06:37:50 +0000

    Add markdown reference in test-vs-notebook.py

--- tmp/test-vs-notebook.py
+++ tmp/test-vs-notebook.py
@@ -13,6 +13,7 @@ plt.plot(x, y)
 plt.show()
 #%% [markdown]
 # # this is markdown
-# - aaa
+# - aaa<br>
+# 参考URL｜https://qiita.com/komiya_____/items/547ae8b5a9b031f18b59#%E5%AE%9F%E8%A1%8C%E6%96%B9%E6%B3%95
 
 # %%
